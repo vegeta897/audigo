720cf659e46ff77b2f60b81117998c1c19843708
vegeta897 <vegeta897@gmail.com>
2018-02-04 14:59:01 -0500

    Fix env port not used, add empty server folder

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const merge = require('lodash/merge');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || NODE_ENV === 'development' ? 3000 : 8080;
+const PORT = process.env.PORT || (NODE_ENV === 'development' ? 3000 : 8080);
 const PROTOCOL = NODE_ENV === 'development' ? 'http://' : 'https://';
 const API_PATH = '/api';
 
